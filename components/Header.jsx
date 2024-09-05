425b841552bf87cfa35f9b292e9bff297a8ddf73
Sagar Pathak <sagarpaathak@gmail.com>
2024-09-05 17:58:14 -0500

    hire me button done

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { Button } from './ui/button'
 
 // components
 import Nav from './Nav'
+import MobileNav from './MobileNav'
 
 
 const Header = () => {
@@ -24,7 +25,7 @@ const Header = () => {
 
                 {/* mobile nav */}
                 <div className="xl:hidden">
-                    mobile nav
+                    <MobileNav />
                 </div>
             </div>
         </header>
--- components/ui/StairTransition.jsx
+++ components/ui/StairTransition.jsx
@@ -13,6 +13,13 @@ const StairTransition = () => {
             <Stairs></Stairs>
         </div>
       </div>
+      <motion.div className='h-screen w-screen fixed bg-primary top-0 pointer-events-none' initial={{opacity: 1}}
+      animate={{
+        opacity: 0, transition: {
+            delay: 1, duration: 0.4, ease: 'easeInOut'
+        }
+      }}
+      ></motion.div>
     </AnimatePresence>
   )
 }
